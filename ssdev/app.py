47d16f0f293d9e658a081d1ebed7733402c155bf
sebszopa <84479357+sebszopa@users.noreply.github.com>
2024-11-24 20:02:21 +0000

    MongoDB connection for patients data
    - MongoDB connection
    - listing patient data form MognoDB, data paging, more patient details
    - matching related  patient id in MongoDB with user data in SQLite 
    - patient register form
    - restricted area for logged users to get access for patients information.

--- ssdev/app.py
+++ ssdev/app.py
@@ -1,21 +1,29 @@
-# !!! Test system user test1@test2.com pass test1234
-
 from flask import Flask, render_template, request, jsonify, redirect, url_for, abort, session, flash, get_flashed_messages
 from bcrypt import hashpw, gensalt, checkpw
 from functools import wraps
-import sqlite3
+from pymongo import MongoClient
+import pandas as pd
 import os
-# importing repleacing string library
+import sqlite3
 import re
 
 # Databases connection config file
-from config import SQLITE_DB
+from config import MONGO_URI, SQLITE_DB
+
+# Establishing connection with MongoDB
+client = MongoClient(os.getenv("MONGO_URI"))
+
+# Selecting Database
+db = client["ssdev001"]
+
+# Picking up the collection
+mongo_collection = db["patient01"]
 
 app = Flask(__name__)
 app.secret_key = 'uwjs9wksk'
 
 ### LOGGIN SECTION AND SESSIONS ###
-# login in form processing
+# Login form
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -23,16 +31,12 @@ def login_required(f):
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
-
-# login out - end of session
+# logging out
 @app.route('/logout')
 def logout():
-    # Usuń dane użytkownika z sesji
     session.pop('username', None)
     return redirect(url_for('index'))
-
-# checking system user in SQLite databese
-
+# checkig user and password in SQLIte database
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -45,162 +49,299 @@ def login():
             result = cursor.fetchone()
 
             if result and checkpw(password.encode('utf-8'), result[1]):
-                # Autentication - verfied user
                 session['username'] = username
                 return redirect(url_for('index'))
             else:
-                # Unathenticatd user
                 return render_template('index.html', title='Login Failed')
 
     elif request.method == 'GET':
         if 'username' in session:
-            # If user logged in
             return redirect(url_for('index'))
         else:
-            # Show login form
             return render_template('index.html', title='Login')
-
     else:
         abort(405)
-# Ner section - logged in user area onlu
+# route to logged user dashboard
 @app.route('/dashboard')
 @login_required
 def dashboard():
-
-    # Eventually tp change for designeted html logged user page
     return render_template('index.html', username=session['username'])
 
-### END OF LOGN SECTION ###
-
-# Home Page
-# UPDATED - Displaing messeges if they exists
-# Used flash metchod for messeges
 @app.route('/')
 def index():
     messages = get_flashed_messages(with_categories=True)
     return render_template('index.html', messages=messages)
 
-# Displaying system users from SQLite database
-# Changned the method how users data are diplayed
-# New table column - nameed - "More" sendig the hidden form to user details page
-
 @app.route('/system')
 def get_sysusers():
     with sqlite3.connect(SQLITE_DB) as conn:
-        conn.row_factory = sqlite3.Row  # changing from data index to name
+        conn.row_factory = sqlite3.Row
         cursor = conn.cursor()
-        cursor.execute('SELECT "su_id", "email", "name", "surname", "role" FROM "sys_usrs";')
+        cursor.execute('SELECT "su_id", "p_id", "email", "name", "surname", "role" FROM "sys_usrs";')
         rows = cursor.fetchall()
         for row in rows:
-            print(dict(row))  #
+            print(dict(row))  # Debug: Wyświetlanie wyników zapytania w terminalu
     return render_template('sysusers.html', rows=rows)
 
-# Form to add system users
 @app.route('/system/users/add', methods=['GET'])
 def sysuser_add_form():
     return render_template('sysuser_add_form.html')
 
-# Adding user to SQLite database
 @app.route('/system/users/added', methods=['POST'])
 def add_sysuser():
-    # Collecting data from the form
+    # Picking up data from the form
     name = request.form.get('name', '').strip()
     surname = request.form.get('surname', '').strip()
     email = request.form.get('email', '').strip()
-    role = request.form.get('role', 'user').strip()  # Default role
+    role = request.form.get('role', 'user').strip()
+    p_id = request.form.get('p_id', 'p_id').strip()
     password = request.form.get('password', '').strip()
 
-    # Data validation - checking for empty fields
+    # checking if all requred fields are filled
     if not all([name, surname, email, password]):
         return jsonify({"error": "All fields must be filled."}), 400
 
-    # Email form validation
+    # Email format validation
     if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
         return jsonify({"error": "Invalid email format."}), 400
 
-    # Password length validation
+    # Checking the passworod lenght
     if len(password) < 8:
         return jsonify({"error": "The password must be at least 8 characters long."}), 400
 
-    # Password encryption
+    # Encrypting password
     hashed_password = hashpw(password.encode('utf-8'), gensalt())
 
     try:
-        # Establishing connection with SQLite
         with sqlite3.connect(SQLITE_DB) as conn:
             cursor = conn.cursor()
             sql = """
-            INSERT INTO sys_usrs (email, name, surname, role, passwd)
-            VALUES (?, ?, ?, ?, ?)
+            INSERT INTO sys_usrs (email, name, surname, role, passwd, p_id)
+            VALUES (?, ?, ?, ?, ?, ?)
             """
-            values = (email, name, surname, role, hashed_password)
+            values = (email, name, surname, role, hashed_password, p_id)
             cursor.execute(sql, values)
             conn.commit()
 
-            print(f"User {name} {surname} has been added.")
+            print(f"User {name} {surname} has been added.")  # user adding confirmation
 
     except sqlite3.IntegrityError as e:
-        print(f"Duplicate email: {e}")
+        print(f"Duplicate email: {e}")  # Debug: checking for duplicated emails
         return jsonify({"error": "The email already exists."}), 400
 
     except sqlite3.Error as e:
-        logging.exception("Database error occurred")
-        return jsonify({"error": "There was an error whie adding the data."}), 500
+        logging.exception("Database error occurred")  # Database errors message
+        return jsonify({"error": "There was an error while adding the data."}), 500
 
     return render_template('sysuser_added.html', name=name, surname=surname, email=email, role=role)
 
-# New feture - updating users Details
+# Fuctions updating sususer details and picking up data related data form MongoDB
 @app.route('/system/users/more', methods=['GET', 'POST'])
 def user_more_details():
     su_id = request.form.get('su_id')
 
-# checking if su_id has been post
     if su_id is None:
-        flash("No user id provided", 400)
+        flash("No user id provided", "error")
         return redirect(url_for('index'))
 
-# collecting one user details form SQLite
-# Next feautre - collecting related user data from MongoDB to displau at the same page
+    # Estabishing connectin with MongoDB
+    try:
+        mongo_client = MongoClient(MONGO_URI)
+        mongo_collection = mongo_client["ssdev001"]["patient01"]
 
-    with sqlite3.connect(SQLITE_DB) as conn:
-        conn.row_factory = sqlite3.Row  # Change to names
-        cursor = conn.cursor()
-        cursor.execute('SELECT "su_id", "email", "name", "surname", "role" FROM "sys_usrs" WHERE su_id = ?', (su_id,))
-        row = cursor.fetchone()  # Picking up just one row
-        if row:
-            return render_template('sysuser_mod_form.html', row=row)  # rendering row details
-        else:
-            flash("User not found", 404)
-            return redirect(url_for('index'))
+        # SQLite connection
+        with sqlite3.connect(SQLITE_DB) as conn:
+            conn.row_factory = sqlite3.Row
+            cursor = conn.cursor()
 
-### UPDATING USERR PASSWORD IN SQLite ###
+            cursor.execute('SELECT "su_id", "email", "name", "surname", "role", "p_id" FROM "sys_usrs" WHERE su_id = ?', (su_id,))
+            row = cursor.fetchone()
+
+            if not row:
+                flash("User not found", "error")
+                return redirect(url_for('index'))
+
+            user_data = dict(row)
+            p_id = user_data.get("p_id")
+
+            if p_id:
+                # Mapping data form MongoDB for realted SQLite user is
+                mongo_field_mapping = {
+                    "id": "p_id",
+                    "gender": "gender",
+                    "age": "age",
+                    "hypertension": "has_hypertension",
+                    "heart_disease": "has_heart_disease",
+                    "ever_married": "marital_status",
+                    "work_type": "work_type",
+                    "Residence_type": "residence_type",
+                    "avg_glucose_level": "avg_glucose_level",
+                    "bmi": "bmi",
+                    "smoking_status": "smoking_status",
+                    "stroke": "stroke_history"
+                }
+
+                mongo_data = mongo_collection.find_one({"id": p_id}, {"_id": 0})
+                if mongo_data:
+                    mapped_data = {mongo_field_mapping[key]: value for key, value in mongo_data.items() if key in mongo_field_mapping}
+                    user_data.update(mapped_data)
+
+        return render_template('sysuser_mod_form.html', row=user_data)
+
+    except Exception as e:
+        flash(f"Error: {str(e)}", "error")
+        return redirect(url_for('index'))
+
+# SysUser updadt pasword function
 @app.route('/system/users/update_pwd', methods=['POST'])
 def update_password():
-    # Collecting password from form
     pwd1 = request.form.get('pwd1')
     pwd2 = request.form.get('pwd2')
     su_id = request.form.get('su_id')
 
-    # Checking password missmatch
+    # Matiching passwords
     if pwd1 != pwd2:
+        # Flash message if passwords missmatch
         flash("Passwords do not match. Please try again.", "error")
         return redirect(url_for('index'))
 
-    # Encoding password
+    # Encrypting paswords
     hashed_pwd = hashpw(pwd1.encode('utf-8'), gensalt())
 
-    # Updating pwd field in SQLite databes with matching su_id
     with sqlite3.connect(SQLITE_DB) as conn:
         cursor = conn.cursor()
         try:
             cursor.execute("UPDATE sys_usrs SET passwd = ? WHERE su_id = ?", (hashed_pwd, su_id))
             conn.commit()
-            flash("Password updated successfully.", "success")
+            flash("Password updated successfully.", "success")  # Flash message - passwodr update success
         except sqlite3.Error as e:
             flash(f"An error occurred: {e}", "error")
             return redirect(url_for('index'))
 
     return redirect(url_for('index'))
+
+### Patients area ####
+# Liting patients form MongoDB
+
+@app.route('/patients')
+def patients():
+    try:
+        # Patient data pagination
+        page = int(request.args.get('page', 1))  # Domyślna wartość to 1
+        per_page = 20  # Liczba rekordów na stronę
+
+        # MongoDB connection
+        mongo_client = MongoClient(MONGO_URI)
+        db = mongo_client["ssdev001"]
+        collection = db["patient01"]
+
+        # Data listing
+        data = list(collection.find())
+        for doc in data:
+            doc['_id'] = str(doc['_id'])  # Zamiana ObjectId na string
+
+        mongo_client.close()
+
+        # Data convertion, assingling to the relevant columns
+        if data:
+            df = pd.DataFrame(data)
+            columns_to_display = ['id', 'gender', 'age', 'work_type', 'Residence_type', 'bmi', 'smoking_status', 'stroke']
+            df = df[columns_to_display]
+
+            # Converting defoult row headers to names
+            column_headers = {
+                'id': 'Patient ID',
+                'gender': 'Gender',
+                'age': 'Age',
+                'work_type': 'Work Type',
+                'Residence_type': 'Residence Type',
+                'bmi': 'BMI',
+                'smoking_status': 'Smoking Status',
+                'stroke': 'Stroke'
+            }
+
+            headers = [column_headers[col] for col in columns_to_display]
+
+            total_records = len(df)
+            total_pages = (total_records + per_page - 1) // per_page  # Liczba stron
+
+            # Pagination
+            start_idx = (page - 1) * per_page
+            end_idx = start_idx + per_page
+            df_page = df.iloc[start_idx:end_idx]
+
+            users_data = df_page.to_dict(orient='records')
+        else:
+            users_data = []
+            total_pages = 1
+
+        # Passing data to the html template
+        return render_template(
+            'patients.html',
+            users_data=users_data,
+            page=page,
+            total_pages=total_pages,
+            headers=headers,
+            columns=columns_to_display
+        )
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@app.route('/patient_details/<patient_id>')
+def patient_details(patient_id):
+    try:
+        # ModogoDB connectin
+        mongo_client = MongoClient(MONGO_URI)
+        db = mongo_client["ssdev001"]
+        collection = db["patient01"]
+
+        # Converting p_id form string to numbaer
+        try:
+            patient_id = int(patient_id)
+        except ValueError:
+            return "Invalid patient ID", 400
+
+        # Picking up the daa for MongoDB besed on patient id not document _id
+        patient_data = collection.find_one({'id': patient_id})
+
+        mongo_client.close()
+
+        if patient_data:
+            # remówing _id field from displaing
+            patient_data.pop('_id', None)
+
+            return render_template('patient_details.html', patient=patient_data)
+        else:
+            return "Patient not found", 404
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@app.route('/patient_register')
+def patient_register():
+    return render_template('patient_rform.html')
+
+# MongoDB connection test
+@app.route('/mongo_test')
+def mongo_test():
+
+    try:
+        mongo_client = MongoClient(MONGO_URI)
+        db = mongo_client["ssdev001"]
+        collection = db["patient01"]
+
+        data = list(collection.find())
+        for doc in data:
+            doc['_id'] = str(doc['_id'])
+
+        mongo_client.close()
+
+        return f"<pre>{data}</pre>"
+
+    except Exception as e:
+
+        return f"Error: {str(e)}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081, debug=True, use_reloader=False)
--- ssdev/config.py
+++ ssdev/config.py
@@ -2,3 +2,11 @@
 
 # Ścieżka do bazy danych SQLite
 SQLITE_DB = "ssdev.db"
+
+# URI do MongoDB (możesz wstawić swoje dane)
+
+MONGO_URI = "mongodb+srv://seb_mdb:SsDev001Project@sebmdb0.cjlzg.mongodb.net/?retryWrites=true&w=majority&appName=SebMdb0"
+
+#MONGO_URI = "mongodb+srv://sebaszopa:pDMqwH902j1Y7P53@ssdev01.v5dcg.mongodb.net/?retryWrites=true&w=majority&appName=ssdev001"
+# mongodb+srv://sebaszopa:pDMqwH902j1Y7P53@ssdev01.v5dcg.mongodb.net/?retryWrites=true&w=majority&appName=ssdev01"
+# pass SsDev001Project u seb_mdb
